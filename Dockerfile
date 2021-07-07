64c07c97472258ec3532d3b8570fb4fa00ed67df
Anthony Palumbo <anthonypalumbo82@gmail.com>
2021-07-07 11:52:44 +0900

    Initial Commit. Basic React app with Twilio video capability in docker container.

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM node:16.3-alpine
+WORKDIR /tellalife
+ENV PATH="./node_modules/.bin:$PATH"
+COPY . .
+RUN npm run build
+# CMD ["npm", "start"]
---
+++ src/components/Room.js
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from "react";
+import Participant from "./Participant";
+
+const Room = ({ roomName, username, room, handleLogout, guestRoom }) => {
+  const [participants, setParticipants] = useState([]);
+  const guestURL = window.location.protocol 
+                   + "//" + window.location.host
+                   + "/?guestRoom=" + guestRoom;
+
+  useEffect(() => {
+    const participantConnected = (participant) => {
+      setParticipants((prevParticipants) => [...prevParticipants, participant]);
+    };
+
+    const participantDisconnected = (participant) => {
+      setParticipants((prevParticipants) =>
+        prevParticipants.filter((p) => p !== participant)
+      );
+    };
+
+    room.on("participantConnected", participantConnected);
+    room.on("participantDisconnected", participantDisconnected);
+    room.participants.forEach(participantConnected);
+    return () => {
+      room.off("participantConnected", participantConnected);
+      room.off("participantDisconnected", participantDisconnected);
+    };
+  }, [room]);
+
+  const remoteParticipants = participants.map((participant) => (
+    <Participant key={participant.sid} participant={participant} />
+  ));
+
+  return (
+    <div className="room">
+      <h4>Room: {roomName}</h4>
+      
+      {/* <h5>Remote Participants</h5> */}
+      <div className="remote-participants">{remoteParticipants}</div>
+      <div className="local-participant">
+        {room ? (
+          <Participant
+            key={room.localParticipant.sid}
+            participant={room.localParticipant}
+          />
+        ) : (
+          ""
+        )}
+        {/* <p>Invite others to join at the following link</p>
+        <p>{guestURL}</p> */}
+        <button onClick={handleLogout}>Leave Room</button>
+      </div>
+    </div>
+  );
+};
+
+export default Room;
