c7b6a47ab7f45b361649a27d1b3cce956eb6e769
Anthony Palumbo <anthonypalumbo82@gmail.com>
2021-07-09 01:15:08 +0900

    Fixed error in src/App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import VideoChat from "./components/VideoChat";
 import axios from "axios";
 
-import React, { useEffect, useState } from "react";
 
 function App () {
 	useEffect(() => {
