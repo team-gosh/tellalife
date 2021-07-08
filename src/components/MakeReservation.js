5e30b07b8ac8d0acf374d04f47f5ae37b2d68c47
Anthony Palumbo <anthonypalumbo82@gmail.com>
2021-07-08 23:59:45 +0900

    Added MakeReservation component.

---
+++ src/components/MakeReservation.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+
+function MakeReservation(props) {
+  const {
+    user,
+  } = props;
+
+  return (
+    <div className='MakeReservation'>
+      <h3>MakeReservation</h3>
+      <button>Submit</button>
+    </div>
+  );
+} 
