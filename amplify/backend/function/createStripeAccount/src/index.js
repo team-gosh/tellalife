9379e993522d8bf0b67d9ba38bce6eed85f3e6d6
Miho Ogura <pluto04@live.jp>
2021-07-13 18:49:56 +0900

    Changed the env

--- amplify/backend/function/createStripeAccount/src/index.js
+++ amplify/backend/function/createStripeAccount/src/index.js
@@ -1,7 +1,5 @@
 const secretKey = process.env.REACT_APP_STRIPE_API_SECRET;
-const stripe = require("stripe")(
-	"sk_test_51J9oYtITm2RX3fVqDlxAFvVcTcHeRuCUoyPkaD13a1W11CTEtv5aKNIwLw9vJ5cmEUTIeKzFPJJjxv75ujMraEcb002w3ZloIO"
-);
+const stripe = require("stripe")(secretKey);
 
 exports.handler = async function (event, context, callback) {
 	const account = await stripe.accounts.create({
