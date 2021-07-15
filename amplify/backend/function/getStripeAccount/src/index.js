49dbc315c179cf8f95ce64ddc0b9c0ee00ee1196
Miho Ogura <pluto04@live.jp>
2021-07-15 18:03:16 +0900

    Changed env

--- amplify/backend/function/getStripeAccount/src/index.js
+++ amplify/backend/function/getStripeAccount/src/index.js
@@ -1,6 +1,5 @@
-// const secretKey = process.env.REACT_APP_STRIPE_API_SECRET;
-const secretKey =
-	"sk_test_51J9oYtITm2RX3fVqDlxAFvVcTcHeRuCUoyPkaD13a1W11CTEtv5aKNIwLw9vJ5cmEUTIeKzFPJJjxv75ujMraEcb002w3ZloIO	";
+const secretKey = process.env.REACT_APP_STRIPE_API_SECRET;
+
 const stripe = require("stripe")(secretKey);
 
 exports.handler = async function (event, context, callback) {
