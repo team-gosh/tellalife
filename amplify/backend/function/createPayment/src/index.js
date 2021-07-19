b02f6c6a53fbfa9b89e5fadbb8ec36a1f1864dfc
Anthony Palumbo <anthonypalumbo82@gmail.com>
2021-07-19 09:27:04 +0900

    updated schema to have seen in AttendingUsers table

--- amplify/backend/function/createPayment/src/index.js
+++ amplify/backend/function/createPayment/src/index.js
@@ -1,3 +1,5 @@
+// const secretKey = process.env.REACT_APP_STRIPE_API_SECRET;
+
 const secretKey = process.env.REACT_APP_STRIPE_API_SECRET;
 
 const stripe = require("stripe")(secretKey);
@@ -9,9 +11,12 @@ exports.handler = async function (event, context, callback) {
 		payment_method_types: [ "card" ],
 		application_fee_amount: 123,
 		transfer_data: {
-			destination: event.arguments.input.stripeAccount,
+			// destination: event.arguments.input.stripeAccount,
+			destination: "acct_1JDe4ERPtTBKeA1Y",
 		},
 	});
+
+	console.log(paymentIntent);
 
 	return paymentIntent.client_secret;
 };
